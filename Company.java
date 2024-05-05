f10d090035b5f232a3eef17b709ab2df891a8321
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-05-05 10:44:22 +0800

    new file

--- Company.java
+++ Company.java
@@ -6,15 +6,29 @@ import employee.Manager;
 public class Company {
     public static void main(String[] args) {
        
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Employee("Anna Crown", 3000);
         employees[1] = new Employee("Suisan White", 3500);
         employees[2] = new Employee("Emma P", 4000);
         employees[3] = new Employee("Jacob G", 3200);
         employees[4] = new Employee("Eva D", 3800);
+        employees[5] = new Employee("Maria D", 3800);
+        employees[6] = new Employee("Kate D", 3800);
 
-        System.out.println("Dane dla wszystkich pracowników:");
+        for (Employee employee : employees) {
+            double newSalary = employee.getSalary() + 500;
+            employee.setSalary(newSalary);
+        }
+
+        for (Employee employee : employees) {
+            if (employee instanceof Manager) {
+                ((Manager) employee).setNumberOfSubordinates(2); 
+                employee.setSalary(7500);
+            }
+        }
+
+        System.out.println("Zaktualizowane informacje o pracownikach:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
