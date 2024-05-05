15418fbc4c91c6d330edf543a6852030d855f4da
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-05-05 09:16:53 +0800

    new files

--- Company.java
+++ Company.java
@@ -1,33 +1,22 @@
 public class Company {
-    public class Employee {
-        private String fullName;
-        private double salary;
-    
-    
-        public Employee(String fullName, double salary) {
-            this.fullName = fullName;
-            this.salary = salary;
-        }
-    
-     
-        public String getName() {
-            return fullName;
-        }
-    
-     
-        public double getSalary() {
-            return salary;
-        }
-    
-     
-        public void setSalary(double newSalary) {
-            this.salary = newSalary;
-        }
-    
-    
-        @Override
-        public String toString() {
-            return "Employee: {" + fullName + "}, Salary: {" + salary + "}";
+    public static void main(String[] args) {
+       
+        Employee[] employees = new Employee[5];
+
+        employees[0] = new Employee("Anna Crown", 3000);
+        employees[1] = new Employee("Suisan White", 3500);
+        employees[2] = new Employee("Emma P", 4000);
+        employees[3] = new Employee("Jacob G", 3200);
+        employees[4] = new Employee("Eva D", 3800);
+
+        System.out.println("Dane dla pracownika o indeksie 3:");
+        System.out.println(employees[3]);
+
+        employees[3].setSalary(3500);
+
+        System.out.println("\nDane dla wszystkich pracownikow:");
+        for (Employee employee : employees) {
+            System.out.println(employee);
         }
     }
 }
--- Employee.java
+++ Employee.java
@@ -1,39 +1,27 @@
 public class Employee {
-    public class Employee {
-        public Employee(String string, int i) {
-        
-        }
-    
-        public static void main(String[] args) {
-    
-            Employee[] employees = new Employee[5];
-    
-       
-            employees[0] = new Employee("Anna Cwown", 3000);
-            employees[1] = new Employee("Jacob Smith", 3500);
-            employees[2] = new Employee("Magda Johnson", 4000);
-            employees[3] = new Employee("Jan Brown", 3200);
-            employees[4] = new Employee("Emma White", 3800);
-    
-          
-            System.out.println("Dane dla pracownika o indeksie 3:");
-            System.out.println(employees[3]);
-    
-          
-            employees[3].setSalary(3500);
-    
-        
-            System.out.println("\nDane dla wszystkich pracownikow:");
-            for (Employee employee : employees) {
-                System.out.println(employee);
-            }
-        }
-    
-        private void setSalary(int i) {
-        
-            throw new UnsupportedOperationException("Unimplemented method 'setSalary'");
-        }
+    private String fullName;
+    private double salary;
+
+
+    public Employee(String fullName, double salary) {
+        this.fullName = fullName;
+        this.salary = salary;
     }
-    
-    
+
+    public String getName() {
+        return fullName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double newSalary) {
+        this.salary = newSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: {" + fullName + "}, Salary: {" + salary + "}";
+    }
 }
