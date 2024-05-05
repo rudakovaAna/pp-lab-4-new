fca95f4ba39d3763a2fdca2096662146dcbc14b2
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-05-05 10:27:30 +0800

    new file

--- Company.java
+++ Company.java
@@ -14,6 +14,17 @@ public class Company {
         employees[3] = new Employee("Jacob G", 3200);
         employees[4] = new Employee("Eva D", 3800);
 
+        int nonManagerEmployeesCount = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                nonManagerEmployeesCount++;
+            }
+        }
+
+        ((Manager)employees[0]).setNumberOfSubordinates(nonManagerEmployeesCount);
+
+        employees[0].setSalary(7500);
+
         System.out.println("Dane dla wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
